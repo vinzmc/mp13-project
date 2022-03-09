2f56a92422cbc6b1a73a33e15da2fe996832e78b
Maurice Marvin <maurice.marvin8812@gmail.com>
2022-03-09 19:26:10 +0700

    Update product controller
    1. implementing new helper class
    2. removing all toResponse method (migrated to helper class)
    3. implementing new method
    - update
    -delete

--- server/src/main/java/com/haibca/server/web/controller/ProductController.java
+++ server/src/main/java/com/haibca/server/web/controller/ProductController.java
@@ -1,16 +1,15 @@
 package com.haibca.server.web.controller;
 
-import com.haibca.server.entity.Category;
 import com.haibca.server.entity.Product;
+import com.haibca.server.helper.impl.ProductResponseConstructorImpl;
 import com.haibca.server.service.ProductService;
 import com.haibca.server.validation.ProductExists;
 import com.haibca.server.web.model.Response;
-import com.haibca.server.web.model.category.CategoryResponse;
 import com.haibca.server.web.model.product.CreateProductRequest;
 import com.haibca.server.web.model.product.ProductResponse;
+import com.haibca.server.web.model.product.UpdateProductRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,7 +17,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.LinkedList;
 import java.util.List;
 
 
@@ -30,6 +28,10 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+    @Autowired
+    ProductResponseConstructorImpl productResponseConstructor;
+
+
     @Operation(summary = "Create new Product")
     @PostMapping(
             path = "/api/products",
@@ -39,7 +41,7 @@ public class ProductController {
         Product product = productService.create(request);
         return Response.<ProductResponse>builder()
                 .status(HttpStatus.OK.value())
-                .data(toResponse(product))
+                .data(productResponseConstructor.toResponse(product))
                 .build();
     }
 
@@ -51,7 +53,7 @@ public class ProductController {
         List<Product> product = productService.findAll();
         return Response.<List<ProductResponse>>builder()
                 .status(HttpStatus.OK.value())
-                .data(toResponse(product))
+                .data(productResponseConstructor.toResponse(product))
                 .build();
     }
 
@@ -63,36 +65,33 @@ public class ProductController {
         Product product = productService.findById(id);
         return Response.<ProductResponse>builder()
                 .status(HttpStatus.OK.value())
-                .data(toResponse(product))
+                .data(productResponseConstructor.toResponse(product))
                 .build();
     }
 
-    //product list response untuk product list
-    private List<ProductResponse> toResponse(List<Product> product) {
-        List<ProductResponse> responses = new LinkedList<ProductResponse>();
-        for (Product element:product) {
-            ProductResponse productResponse = ProductResponse.builder()
-                    .category(toResponse(element.getCategory()))
-                    .build();
-            BeanUtils.copyProperties(element, productResponse);
-            responses.add(productResponse);
-        }
-
-        BeanUtils.copyProperties(product, responses);
-        return responses;
-    }
-
-    private ProductResponse toResponse(Product product) {
-        ProductResponse productResponse = ProductResponse.builder()
-                .category(toResponse(product.getCategory()))
+    @Operation(summary = "Update product.")
+    @PutMapping(
+            path = "/api/products/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response<ProductResponse> update(@ProductExists @PathVariable Integer id,
+                                            @Valid @RequestBody UpdateProductRequest request) {
+        Product product = productService.update(id, request);
+        return Response.<ProductResponse>builder()
+                .status(HttpStatus.OK.value())
+                .data(productResponseConstructor.toResponse(product))
                 .build();
-        BeanUtils.copyProperties(product, productResponse);
-        return productResponse;
     }
 
-    private CategoryResponse toResponse(Category category) {
-        CategoryResponse categoryResponse = CategoryResponse.builder().build();
-        BeanUtils.copyProperties(category, categoryResponse);
-        return categoryResponse;
+    @Operation(summary = "Delete product by id.")
+    @DeleteMapping(
+            path = "/api/products/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response<Boolean> deleteById(@ProductExists @PathVariable Integer id) {
+        productService.deleteById(id);
+        return Response.<Boolean>builder()
+                .status(HttpStatus.OK.value())
+                .data(true)
+                .build();
     }
 }
