e001a8cfdc8e927430d86d11ea9d0f9398fcfd42
Maurice Marvin <maurice.marvin8812@gmail.com>
2022-03-10 20:52:57 +0700

    removing unused annotation

--- server/src/main/java/com/haibca/server/configuration/CorsConfiguration.java
+++ server/src/main/java/com/haibca/server/configuration/CorsConfiguration.java
@@ -1,12 +1,10 @@
 package com.haibca.server.configuration;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Slf4j
 @Configuration
 public class CorsConfiguration {
     @Bean
